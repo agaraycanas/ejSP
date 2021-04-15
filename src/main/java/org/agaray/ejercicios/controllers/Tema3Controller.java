9a283af0f28c81a06f886b342cd53881b9031975
Alberto Garay (taller3) <garay_alberto@hotmail.com>
2021-04-15 11:01:22 +0200

    Tema 3, ej 12 (comenzando)

--- src/main/java/org/agaray/ejercicios/controllers/Tema3Controller.java
+++ src/main/java/org/agaray/ejercicios/controllers/Tema3Controller.java
@@ -1,11 +1,13 @@
 package org.agaray.ejercicios.controllers;
 
 //import static java.util.Map.entry; 
-import java.util.Map;
-import org.agaray.ejercicios.helpers.H;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,11 +15,49 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/t3")
 public class Tema3Controller {
 
-	@GetMapping("ej12")
-	public String ej12(ModelMap m) {
+	@GetMapping("ej12 ej12/1")
+	public String ej12_1(ModelMap m, HttpSession s) {
+		s.invalidate();
 		m.put("view", "t3/ej12/uno");
 		return "_t/frame";
 	}
+	
+	@PostMapping("ej12/2")
+	public String ej12_2(
+			@RequestParam(value="nPasos",required = false) Integer nPasos,
+			@RequestParam(value="n",required = false) Integer n,
+			HttpSession s
+			) {
+			s.setAttribute("n", s.getAttribute("n")!=null ? s.getAttribute("n") : nPasos );
+		
+		return null;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 
 }
