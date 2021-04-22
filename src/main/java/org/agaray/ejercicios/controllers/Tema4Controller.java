31a7ac767f128859063cb6213c0a6b17bbd57a83
Alberto Garay (taller3) <garay_alberto@hotmail.com>
2021-04-22 10:56:23 +0200

    Ej 4 - 1

--- src/main/java/org/agaray/ejercicios/controllers/Tema4Controller.java
+++ src/main/java/org/agaray/ejercicios/controllers/Tema4Controller.java
@@ -1,10 +1,13 @@
 package org.agaray.ejercicios.controllers;
 
 
+import java.util.Random;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/t4")
@@ -17,7 +20,27 @@ public class Tema4Controller {
 	}
 
 	@GetMapping("ej1/a")
-	public String ej1AJAX() {
+	public String ej1AJAX(
+			@RequestParam(value="nombre",required = false) String nombre,
+			ModelMap m
+			) {
+		nombre = (nombre!=null ? nombre : "desconocido");
+		m.put("mensaje", this.getMensaje(nombre));
 		return "t4/ej01/ajax";
+	}
+	
+	private String getMensaje(String nombre) {
+		int num = (new Random()).nextInt(3);
+		String ini = "";
+		String fin = "";
+		
+		switch (num) {
+			case 0: ini = "¡¡ Qué pasa";fin=" !!";break;
+			case 1: ini ="Hola";break;
+			case 2:ini = "Qué tal estás";break;
+		}
+		String mensaje = ini + " " + nombre + " " + fin;
+		
+		return mensaje;
 	}
 }
